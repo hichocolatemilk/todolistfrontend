aa4dfe06b211ff97aa7b6d4f9f2d71aff970bf2d
hichocolatemilk <111757770+hichocolatemilk@users.noreply.github.com>
2023-04-03 22:49:33 +0900

    logout 기능 개선
    logout 기능 개선

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,17 +7,39 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
+import { removeCookie } from "./Cookie";
 
 export default function Appbar() {
+  const [isLogged, setLogged] = useState(false);
+
+  useEffect(() => {
+    checkStorage();
+  }, [isLogged]);
+
   let navigate = useNavigate();
 
-  function onLogin() {
-    navigate("/Login");
-  }
+  const checkStorage = () => {
+    if (localStorage.getItem("accesstoken")) {
+      setLogged(true);
+    } else {
+      setLogged(false);
+    }
+  };
 
-  function onHome() {
+  const onLogin = () => {
+    navigate("/Login");
+  };
+  const onLogout = () => {
+    localStorage.removeItem("accesstoken");
+    removeCookie("accesstoken");
+    setLogged(false);
+    window.location.reload();
+  };
+
+  const onHome = () => {
     navigate("/");
-  }
+    window.location.reload();
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -36,9 +58,15 @@ export default function Appbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             ToDoList
           </Typography>{" "}
-          <Button color="inherit" onClick={onLogin}>
-            Login
-          </Button>
+          {!isLogged ? (
+            <Button color="inherit" onClick={onLogin}>
+              Login
+            </Button>
+          ) : (
+            <Button onClick={onLogout} color="inherit">
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
